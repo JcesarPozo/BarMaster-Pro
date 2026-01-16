db1d7cc2041c91cd8d244583bfb65dbd7c12b955
JcesarPozo <jcdepr64@gmail.com>
2026-01-16 10:57:55 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
+  css: {
+    postcss: './postcss.config.cjs', // Le decimos exactamente dónde está el config
   }
 })
