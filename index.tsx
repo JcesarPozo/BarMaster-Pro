756bc66d7683e63a2f2143168092324edd29b7cd
JcesarPozo <jcdepr64@gmail.com>
2026-01-16 09:48:31 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import './index.css'; // <--- ASEGÚRATE DE QUE ESTA LÍNEA ESTÉ AQUÍ
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
